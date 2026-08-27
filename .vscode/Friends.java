4a48f27f655261eec15612580990ef11c5380182
karishma08 <karishmanadaf87@gmail.com>
2026-08-27 10:14:46 +0530

    Activity 1 and 2

---
+++ .vscode/Friends.java
@@ -0,0 +1,7 @@
+class Friends{
+    public static void main (String[] args) {
+        System.out.println("Karishma");
+        System.out.println("Jyoti");
+    }
+
+}
---
+++ activity 1/hellocpp.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // Required for std::cout
+int main() {
+   std::cout << "Hello World"; // Output to console
+   return 0; // Exit status
+}
